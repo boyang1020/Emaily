ede4ff55507acca4ec6c1d0c6d3584aaa597e921
boyang1020 <boyang1020@gmail.com>
2018-11-08 18:53:19 -0800

    set up route in prod

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ const bodyParser = require('body-parser');
 require('./models/user');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+mongoose.connect(
+	keys.mongoURI,
+	{ useNewUrlParser: true }
+);
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+	})
 );
 
 app.use(passport.initialize());
@@ -26,16 +29,16 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets 
-    // like our main.js file, or main.css file
-    app.use(express.static('client/build'));
+	// Express will serve up production assets
+	// like our main.js file, or main.css file
+	app.use(express.static('client/build'));
 
-    // Express will serve up the index.html file 
-    // if it doesn't recogize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+	// Express will serve up the index.html file
+	// if it doesn't recogize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
