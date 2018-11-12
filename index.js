fa4c879604cdbba26c6ca6effe9a2fc3be471844
boyang1020 <boyang1020@gmail.com>
2018-11-11 22:29:14 -0800

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
-require('./models/user');
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(
