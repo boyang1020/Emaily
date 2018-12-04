8acaf19a2201894e19da6b1c8bed5d7f12f36ae0
boyang1020 <boyang1020@gmail.com>
2018-12-03 22:12:39 -0800

    Mailer constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,15 @@ const helper = sendgrid.mailer;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+ constructor({ sunject, recipients }, content){
+    super();
 
+     this.from_email = new helper.Email('no-reply@emaily.com');
+     this.subject = subject;
+     this.body = new helper.Content('text/html', content);
+     this.recipients = this.formatAddresses(recipients); 
+ }
 }
 
+new Mailer()
 nodule.exports = Mailer;
