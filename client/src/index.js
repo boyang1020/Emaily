0be0e58c41a334b712069344eaf5a8d5a980ba00
boyang1020 <boyang1020@gmail.com>
2018-12-05 19:17:00 -0800

    improving the email template

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk'
 import reducers from './reducers';
 
+
 import App from './components/App';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
